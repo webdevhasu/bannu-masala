66690deb066ef02311abae6754d574da0d0e3329
webdevhasu <webdevhasu@github.com>
2026-06-17 20:17:32 +0500

    Add automatic redirect to bannumasala.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://bannumasala.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 export default nextConfig;
